26b7ac9798c102f25d2471758bdd909e38efb423
Shubham <shubh622005@gmail.com>
2023-08-29 22:25:24 +0530

    .

--- server.js
+++ server.js
@@ -1,20 +1,14 @@
 const express = require('express');
-const http = require('http');
-const socketio = require('socket.io');
+// const http = require('http');
 const connectDb = require('./config/dbConnection');
 const errorHandler = require('./middleware/errorHandler');
 const dotenv = require('dotenv').config();
 const cors = require('cors');
-const corsOptions = {
-    origin: '*',
-    methods: ['GET', 'POST'],
-  };  
 connectDb();
 const app = express();
 const port = process.env.PORT || 5000;
 app.use(express.json());
-app.use(cors(corsOptions));
-const server = http.createServer(app);
+app.use(cors());
 app.use('/api/auth', require("./routes/authRoutes"));
 app.use('/api/livekit', require("./routes/livekitRoutes"));
 app.use('/api/admin', require("./routes/adminRoutes"));
