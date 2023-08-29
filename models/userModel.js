b7c1d4e2d431e24924ad79c8e0d265d25152cd29
Shubham <shubh622005@gmail.com>
2023-08-29 22:42:04 +0530

    .

--- models/userModel.js
+++ models/userModel.js
@@ -49,7 +49,8 @@ const userSchema = new mongoose.Schema(
             default: ""
         },
         isSpeaking:{
-            type: Boolean
+            type: Boolean,
+            default: false,
         }
     },{
         timestamps: true,
