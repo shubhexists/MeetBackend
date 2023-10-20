908ca41a8b82c40d9dbd1f3c00f30b251d228197
Shubham Singh <shubh622005@gmail.com>
2023-10-21 02:57:34 +0530

    Owner Model Created

---
+++ models/OwnerModel.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const OwnerSchema = new mongoose.Schema(
+    {
+        ownerId: {
+            type: String,
+            required: [true, "Please enter the ownerId."],
+            unique: true
+        },
+        ownerName: {
+            type: String,
+            required: [true, "Please enter the ownerName."],
+        },
+        password:{
+            type: String,
+            required: [true, "Please enter the password."],
+        },
+        role:{
+            type: String,
+            required: [true, "Please enter the role."],
+        },
+        rooms: {
+            type: [String]
+        },
+        isDisabled: {
+            type: Boolean,
+            default: false,
+        },
+        limitOfAdmin: {
+            type: Number,
+            required: [true, "Please enter the limitOfAdmin."],
+        },
+        admins: {
+            type: [String]
+        }
+    }
+);
+
+module.exports = mongoose.model("Owner", OwnerSchema);
